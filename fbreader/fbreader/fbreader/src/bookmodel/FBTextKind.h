6d0b79fb1674189405a154d6a76f29281ae6e55e
Slava Monich <slava.monich@jolla.com>
2016-10-15 13:18:32 +0300

    [fbreader] Added NUM_KINDS to FBTextKind

--- fbreader/fbreader/fbreader/src/bookmodel/FBTextKind.h
+++ fbreader/fbreader/fbreader/src/bookmodel/FBTextKind.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2004-2010 Geometer Plus <contact@geometerplus.com>
+ * Copyright (C) 2016 Slava Monich <slava.monich@jolla.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -65,7 +66,8 @@ enum FBTextKind {
 	H6 = 36,
 	EXTERNAL_HYPERLINK = 37,
 	BOOK_HYPERLINK = 38,
-	BLOCKQUOTE = 39
+	BLOCKQUOTE = 39,
+	NUM_KINDS
 };
 
 #endif /* __FBTEXTKIND_H__ */
