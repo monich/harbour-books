7b7f8b9e973220a7e2cff44f251fd787b2c81d2f
Slava Monich <slava.monich@jolla.com>
2018-01-31 15:00:27 +0200

    [app] Tweaked the selection background

--- app/src/BooksPaintContext.cpp
+++ app/src/BooksPaintContext.cpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2015-2016 Jolla Ltd.
- * Contact: Slava Monich <slava.monich@jolla.com>
+ * Copyright (C) 2015-2018 Jolla Ltd.
+ * Copyright (C) 2015-2018 Slava Monich <slava.monich@jolla.com>
  *
  * You may use this file under the terms of the BSD license as follows:
  *
@@ -14,7 +14,7 @@
  *     notice, this list of conditions and the following disclaimer in
  *     the documentation and/or other materials provided with the
  *     distribution.
- *   * Neither the name of Nemo Mobile nor the names of its contributors
+ *   * Neither the name of Jolla Ltd nor the names of its contributors
  *     may be used to endorse or promote products derived from this
  *     software without specific prior written permission.
  *
@@ -293,7 +293,7 @@ ZLColor BooksPaintContext::realColor(const std::string& aStyle, bool aInvert)
     } else if (aStyle == BOOK_HYPERLINK) {
         return realColor(23, 68, 128, aInvert);
     } else if (aStyle == ZLTextStyle::SELECTION_BACKGROUND) {
-        return realColor(82, 131, 194, aInvert);
+        return realColor(60, 139, 255, aInvert);
     } else if (aStyle == ZLTextStyle::HIGHLIGHTED_TEXT) {
         return realColor(60, 139, 255, aInvert);
     }
