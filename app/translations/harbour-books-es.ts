0ff9832450573277958391000465d5f01bc1b55e
p4moedo <35430331+p4moedo@users.noreply.github.com>
2018-02-01 22:44:01 +0100

    Create harbour-books-es.ts
    Spanish (es) translation

---
+++ app/translations/harbour-books-es.ts
@@ -0,0 +1,213 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es">
+<context>
+    <name></name>
+    <message id="harbour-books-app-name">
+        <source>Books</source>
+        <translation>Libros</translation>
+    </message>
+    <message id="harbour-books-book-view-back">
+        <source>Back to library</source>
+        <translation>Regresar a la librería</translation>
+    </message>
+    <message id="harbour-books-book-view-loading">
+        <source>Loading...</source>
+        <translation>Cargando...</translation>
+    </message>
+    <message id="harbour-books-book-view-formatting">
+        <source>Formatting...</source>
+        <translation>Formateando...</translation>
+    </message>
+    <message id="harbour-books-book-view-applying_larger_fonts">
+        <source>Applying larger fonts...</source>
+        <translation>Aplicando fuentes más grandes...</translation>
+    </message>
+    <message id="harbour-books-book-view-applying_smaller_fonts">
+        <source>Applying smaller fonts...</source>
+        <translation>Aplicando fuentes más pequeñas...</translation>
+    </message>
+    <message id="harbour-books-book-view-cancel_loading">
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message id="harbour-books-book-view-copied_to_clipboard">
+        <source>Copied to clipboard</source>
+        <extracomment>Pop-up notification</extracomment>
+        <translation>Copiado al portapapeles</translation>
+    </message>
+    <message id="harbour-books-shelf-view-about_to_delete_all">
+        <source>Deleting all books</source>
+        <translation>Borrando todos los libros</translation>
+    </message>
+    <message id="harbour-books-shelf-view-no_books">
+        <source>No books</source>
+        <translation>No hay libros disponibles</translation>
+    </message>
+    <message id="harbour-books-shelf-title-placeholder">
+        <source>Enter folder name</source>
+        <extracomment>Placeholder for the bookshelf title</extracomment>
+        <translation>Introduzca el nombre de la carpeta</translation>
+    </message>
+    <message id="harbour-books-storage-removable">
+        <source>Memory card</source>
+        <extracomment>Header label for the memory card</extracomment>
+        <translation>Tarjeta de memoria</translation>
+    </message>
+    <message id="harbour-books-storage-internal">
+        <source>Internal storage</source>
+        <extracomment>Header label for the internal storage</extracomment>
+        <translation>Almacenamiento interno</translation>
+    </message>
+    <message id="harbour-books-storage-book_count" numerus="yes">
+        <source>%0 book(s)</source>
+        <extracomment>Number of books in the storage header</extracomment>
+        <translation>
+            <numerusform>%0 libro</numerusform>
+            <numerusform>%0 libros</numerusform>
+        </translation>
+    </message>
+    <message id="harbour-books-storage-menu-settings">
+        <source>Settings</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Configuración</translation>
+    </message>
+    <message id="harbour-books-storage-menu-scan_downloads">
+        <source>Scan downloads</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Escanear descargas</translation>
+    </message>
+    <message id="harbour-books-storage-menu-delete_everything">
+        <source>Delete all books</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Borrar todos los libros</translation>
+    </message>
+    <message id="harbour-books-storage-view-swipe_left_hint">
+        <source>Swipe left to see what&apos;s on the SD-card</source>
+        <translation>Arrastre hacia la izquierda para ver contenido en la tarjeta de memoria</translation>
+    </message>
+    <message id="harbour-books-import-page-import_n_books" numerus="yes">
+        <source>Import %0 book(s)</source>
+        <extracomment>Dialog header button</extracomment>
+        <translation>
+            <numerusform>Importar %0 libro</numerusform>
+            <numerusform>Importar %0 libros</numerusform>
+        </translation>
+    </message>
+    <message id="harbour-books-import-page-select_books">
+        <source>Select books</source>
+        <extracomment>Dialog header button</extracomment>
+        <translation>Seleccionar libros</translation>
+    </message>
+    <message id="harbour-books-import-page-select_all">
+        <source>Select all</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Seleccionar todo</translation>
+    </message>
+    <message id="harbour-books-import-page-no_new_books_found">
+        <source>No new books found</source>
+        <extracomment>Import page placeholder</extracomment>
+        <translation>No se han encontrado nuevos libros</translation>
+    </message>
+    <message id="harbour-books-book-browser_link-title">
+        <source>Link</source>
+        <extracomment>External link menu title</extracomment>
+        <translation>Enlazar</translation>
+    </message>
+    <message id="harbour-books-book-browser_link-open_in_browser">
+        <source>Open in browser</source>
+        <extracomment>Open link in browser</extracomment>
+        <translation>Abrir enlace en el navegador</translation>
+    </message>
+    <message id="harbour-books-book-browser_link-copy_to_clipboard">
+        <source>Copy to clipboard</source>
+        <extracomment>Copy link to clipboard</extracomment>
+        <translation>Copiar enlace al portapapeles</translation>
+    </message>
+    <message id="harbour-books-settings-page-header">
+        <source>Books</source>
+        <extracomment>Settings page header</extracomment>
+        <translation>Libros</translation>
+    </message>
+    <message id="harbour-books-settings-page-display-section_header">
+        <source>Display</source>
+        <extracomment>Section header for display settings</extracomment>
+        <translation>Pantalla</translation>
+    </message>
+    <message id="harbour-books-settings-page-font_size_label">
+        <source>Font size</source>
+        <extracomment>Slider label</extracomment>
+        <translation>Tamaño de la fuente</translation>
+    </message>
+    <message id="harbour-books-settings-page-font_size_label-default">
+        <source>Default</source>
+        <extracomment>Slider value label for the standard font size</extracomment>
+        <translation>Por defecto</translation>
+    </message>
+    <message id="harbour-books-settings-page-orientation_label">
+        <source>Orientation</source>
+        <extracomment>Combo box label</extracomment>
+        <translation>Orientación</translation>
+    </message>
+    <message id="harbour-books-settings-page-orientation-dynamic">
+        <source>Dynamic</source>
+        <extracomment>Combo box value for dynamic orientation</extracomment>
+        <translation>Dinámica</translation>
+    </message>
+    <message id="harbour-books-settings-page-orientation-portrait">
+        <source>Portrait</source>
+        <extracomment>Combo box value for portrait orientation</extracomment>
+        <translation>Horizontal</translation>
+    </message>
+    <message id="harbour-books-settings-page-orientation-landscape">
+        <source>Landscape</source>
+        <extracomment>Combo box value for landscape orientation</extracomment>
+        <translation>Vertical</translation>
+    </message>
+    <message id="harbour-books-settings-page-media-keys-section_header">
+        <source>Media keys</source>
+        <extracomment>Section header for media keys</extracomment>
+        <translation>Teclas multimedia</translation>
+    </message>
+    <message id="harbour-books-settings-page-volume_up-label">
+        <source>Volume up</source>
+        <extracomment>Combo box label</extracomment>
+        <translation>Subir volumen</translation>
+    </message>
+    <message id="harbour-books-settings-page-volume_down-label">
+        <source>Volume down</source>
+        <extracomment>Combo box label</extracomment>
+        <translation>Bajar volumen</translation>
+    </message>
+    <message id="harbour-books-settings-page-action-none">
+        <source>No action</source>
+        <extracomment>Combo box value for no action</extracomment>
+        <translation>Sin acción</translation>
+    </message>
+    <message id="harbour-books-settings-page-action-previous_page">
+        <source>Previous page</source>
+        <extracomment>Combo box value for previous page action</extracomment>
+        <translation>Página anterior</translation>
+    </message>
+    <message id="harbour-books-settings-page-action-next_page">
+        <source>Next page</source>
+        <extracomment>Combo box value for next page action</extracomment>
+        <translation>Página posterior</translation>
+    </message>
+    <message id="harbour-books-settings-page-removable-section_header">
+        <source>Memory card</source>
+        <extracomment>Section header for memory card settings</extracomment>
+        <translation>Tarjeta de memoria</translation>
+    </message>
+    <message id="harbour-books-settings-page-removable_root-label">
+        <source>Books folder</source>
+        <extracomment>Settings field label</extracomment>
+        <translation>Carpeta de libros</translation>
+    </message>
+    <message id="harbour-books-settings-page-removable_root-description">
+        <source>Leave the folder name empty to scan the entire memory card for books.</source>
+        <extracomment>Settings field description</extracomment>
+        <translation>Deje el nombre de la carpeta en blanco para escanear la tarjeta de memoria entera en busca de libros.</translation>
+    </message>
+</context>
+</TS>
