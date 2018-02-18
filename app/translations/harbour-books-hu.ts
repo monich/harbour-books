0545fc6312f6d2cbd21f4d8a0cfd4f9e1d8d23bf
1Zgp <36508793+1Zgp@users.noreply.github.com>
2018-02-18 20:32:01 +0200

    [app] Create harbour-books-hu.ts
    Hungarian translation for Books.

---
+++ app/translations/harbour-books-hu.ts
@@ -0,0 +1,211 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hu">
+<context>
+    <name></name>
+    <message id="harbour-books-app-name">
+        <source>Books</source>
+        <translation>Könyvek</translation>
+    </message>
+    <message id="harbour-books-book-view-back">
+        <source>Back to library</source>
+        <translation>Vissza a könyvtárhoz</translation>
+    </message>
+    <message id="harbour-books-book-view-loading">
+        <source>Loading...</source>
+        <translation>Töltés...</translation>
+    </message>
+    <message id="harbour-books-book-view-formatting">
+        <source>Formatting...</source>
+        <translation>Formázás...</translation>
+    </message>
+    <message id="harbour-books-book-view-applying_larger_fonts">
+        <source>Applying larger fonts...</source>
+        <translation>Nagyobb betűk alkalmazása...</translation>
+    </message>
+    <message id="harbour-books-book-view-applying_smaller_fonts">
+        <source>Applying smaller fonts...</source>
+        <translation>Kisebb betűk alkalmazása...</translation>
+    </message>
+    <message id="harbour-books-book-view-cancel_loading">
+        <source>Cancel</source>
+        <translation>Mégse</translation>
+    </message>
+    <message id="harbour-books-book-view-copied_to_clipboard">
+        <source>Copied to clipboard</source>
+        <extracomment>Pop-up notification</extracomment>
+        <translation>A vágólapra másolva</translation>
+    </message>
+    <message id="harbour-books-shelf-view-about_to_delete_all">
+        <source>Deleting all books</source>
+        <translation>Minden könyv törlése</translation>
+    </message>
+    <message id="harbour-books-shelf-view-no_books">
+        <source>No books</source>
+        <translation>Nincsenek könyvek</translation>
+    </message>
+    <message id="harbour-books-shelf-title-placeholder">
+        <source>Enter folder name</source>
+        <extracomment>Placeholder for the bookshelf title</extracomment>
+        <translation>Írd be a mappa nevét</translation>
+    </message>
+    <message id="harbour-books-storage-removable">
+        <source>Memory card</source>
+        <extracomment>Header label for the memory card</extracomment>
+        <translation>Memóriakártya</translation>
+    </message>
+    <message id="harbour-books-storage-internal">
+        <source>Internal storage</source>
+        <extracomment>Header label for the internal storage</extracomment>
+        <translation>Belső tárhely</translation>
+    </message>
+    <message id="harbour-books-storage-book_count" numerus="yes">
+        <source>%0 book(s)</source>
+        <extracomment>Number of books in the storage header</extracomment>
+        <translation>
+            <numerusform>%0 könyv</numerusform>
+        </translation>
+    </message>
+    <message id="harbour-books-storage-menu-settings">
+        <source>Settings</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Beállítások</translation>
+    </message>
+    <message id="harbour-books-storage-menu-scan_downloads">
+        <source>Scan downloads</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Letöltések pásztázása</translation>
+    </message>
+    <message id="harbour-books-storage-menu-delete_everything">
+        <source>Delete all books</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Az összes könyv törlése</translation>
+    </message>
+    <message id="harbour-books-storage-view-swipe_left_hint">
+        <source>Swipe left to see what&apos;s on the SD-card</source>
+        <translation>Lapozz balra, hogy lásd, mi van az SD kártyán</translation>
+    </message>
+    <message id="harbour-books-import-page-import_n_books" numerus="yes">
+        <source>Import %0 book(s)</source>
+        <extracomment>Dialog header button</extracomment>
+        <translation>
+            <numerusform>%0 könyv importálása</numerusform>
+        </translation>
+    </message>
+    <message id="harbour-books-import-page-select_books">
+        <source>Select books</source>
+        <extracomment>Dialog header button</extracomment>
+        <translation>Könyvek kijelölése</translation>
+    </message>
+    <message id="harbour-books-import-page-select_all">
+        <source>Select all</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Az összes kijelölése</translation>
+    </message>
+    <message id="harbour-books-import-page-no_new_books_found">
+        <source>No new books found</source>
+        <extracomment>Import page placeholder</extracomment>
+        <translation>Nem találhatók új könyvek</translation>
+    </message>
+    <message id="harbour-books-book-browser_link-title">
+        <source>Link</source>
+        <extracomment>External link menu title</extracomment>
+        <translation>Hivatkozás</translation>
+    </message>
+    <message id="harbour-books-book-browser_link-open_in_browser">
+        <source>Open in browser</source>
+        <extracomment>Open link in browser</extracomment>
+        <translation>Megnyitás böngészőben</translation>
+    </message>
+    <message id="harbour-books-book-browser_link-copy_to_clipboard">
+        <source>Copy to clipboard</source>
+        <extracomment>Copy link to clipboard</extracomment>
+        <translation>Másolás a vágólapra</translation>
+    </message>
+    <message id="harbour-books-settings-page-header">
+        <source>Books</source>
+        <extracomment>Settings page header</extracomment>
+        <translation>Könyvek</translation>
+    </message>
+    <message id="harbour-books-settings-page-display-section_header">
+        <source>Display</source>
+        <extracomment>Section header for display settings</extracomment>
+        <translation>Kijelző</translation>
+    </message>
+    <message id="harbour-books-settings-page-font_size_label">
+        <source>Font size</source>
+        <extracomment>Slider label</extracomment>
+        <translation>Betűméret</translation>
+    </message>
+    <message id="harbour-books-settings-page-font_size_label-default">
+        <source>Default</source>
+        <extracomment>Slider value label for the standard font size</extracomment>
+        <translation>Alapértelmezett</translation>
+    </message>
+    <message id="harbour-books-settings-page-orientation_label">
+        <source>Orientation</source>
+        <extracomment>Combo box label</extracomment>
+        <translation>Tájolás</translation>
+    </message>
+    <message id="harbour-books-settings-page-orientation-dynamic">
+        <source>Dynamic</source>
+        <extracomment>Combo box value for dynamic orientation</extracomment>
+        <translation>Dinamikus</translation>
+    </message>
+    <message id="harbour-books-settings-page-orientation-portrait">
+        <source>Portrait</source>
+        <extracomment>Combo box value for portrait orientation</extracomment>
+        <translation>Álló</translation>
+    </message>
+    <message id="harbour-books-settings-page-orientation-landscape">
+        <source>Landscape</source>
+        <extracomment>Combo box value for landscape orientation</extracomment>
+        <translation>Fekvő</translation>
+    </message>
+    <message id="harbour-books-settings-page-media-keys-section_header">
+        <source>Media keys</source>
+        <extracomment>Section header for media keys</extracomment>
+        <translation>Média gombok</translation>
+    </message>
+    <message id="harbour-books-settings-page-volume_up-label">
+        <source>Volume up</source>
+        <extracomment>Combo box label</extracomment>
+        <translation>Hangerő fel</translation>
+    </message>
+    <message id="harbour-books-settings-page-volume_down-label">
+        <source>Volume down</source>
+        <extracomment>Combo box label</extracomment>
+        <translation>Hangerő le</translation>
+    </message>
+    <message id="harbour-books-settings-page-action-none">
+        <source>No action</source>
+        <extracomment>Combo box value for no action</extracomment>
+        <translation>Nincs művelet</translation>
+    </message>
+    <message id="harbour-books-settings-page-action-previous_page">
+        <source>Previous page</source>
+        <extracomment>Combo box value for previous page action</extracomment>
+        <translation>Előző oldal</translation>
+    </message>
+    <message id="harbour-books-settings-page-action-next_page">
+        <source>Next page</source>
+        <extracomment>Combo box value for next page action</extracomment>
+        <translation>Következő oldal</translation>
+    </message>
+    <message id="harbour-books-settings-page-removable-section_header">
+        <source>Memory card</source>
+        <extracomment>Section header for memory card settings</extracomment>
+        <translation>Memóriakártya</translation>
+    </message>
+    <message id="harbour-books-settings-page-removable_root-label">
+        <source>Books folder</source>
+        <extracomment>Settings field label</extracomment>
+        <translation>Könyvek mappa</translation>
+    </message>
+    <message id="harbour-books-settings-page-removable_root-description">
+        <source>Leave the folder name empty to scan the entire memory card for books.</source>
+        <extracomment>Settings field description</extracomment>
+        <translation>Hagyd üresen a mappanevet a teljes memóriakártya könyvek utáni pásztázásához.</translation>
+    </message>
+</context>
+</TS>
