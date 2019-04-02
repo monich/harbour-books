06e6485b08f2e489ded2baf75e5db2992e83dc23
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2019-04-02 17:40:53 +0800

    Create harbour-books-zh_cn.ts

---
+++ app/translations/harbour-books-zh_cn.ts
@@ -0,0 +1,288 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_cn">
+<context>
+    <name></name>
+    <message id="harbour-books-app-name">
+        <source>Books</source>
+        <translation>书籍</translation>
+    </message>
+    <message id="harbour-books-book-view-back">
+        <source>Back to library</source>
+        <translation>返回图书馆</translation>
+    </message>
+    <message id="harbour-books-book-view-loading">
+        <source>Loading...</source>
+        <translation>正在加载</translation>
+    </message>
+    <message id="harbour-books-book-view-formatting">
+        <source>Formatting...</source>
+        <translation>正在格式化</translation>
+    </message>
+    <message id="harbour-books-book-view-applying_larger_fonts">
+        <source>Applying larger fonts...</source>
+        <translation正在应用更大的字体</translation>
+    </message>
+    <message id="harbour-books-book-view-applying_smaller_fonts">
+        <source>Applying smaller fonts...</source>
+        <translation>正在应用更小的字体</translation>
+    </message>
+    <message id="harbour-books-book-view-cancel_loading">
+        <source>Cancel</source>
+        <translation>取消</translation>
+    </message>
+    <message id="harbour-books-book-view-copied_to_clipboard">
+        <source>Copied to clipboard</source>
+        <extracomment>Pop-up notification</extracomment>
+        <translation>已复制到剪贴板</translation>
+    </message>
+    <message id="harbour-books-shelf-view-about_to_delete_all">
+        <source>Deleting all books</source>
+        <translation>正在删除全部书籍</translation>
+    </message>
+    <message id="harbour-books-shelf-view-no_books">
+        <source>No books</source>
+        <translation>暂无书籍</translation>
+    </message>
+    <message id="harbour-books-shelf-title-placeholder">
+        <source>Enter folder name</source>
+        <extracomment>Placeholder for the bookshelf title</extracomment>
+        <translation>输入文件夹名称</translation>
+    </message>
+    <message id="harbour-books-storage-removable">
+        <source>Memory card</source>
+        <extracomment>Header label for the memory card</extracomment>
+        <translation>储存卡</translation>
+    </message>
+    <message id="harbour-books-storage-internal">
+        <source>Internal storage</source>
+        <extracomment>Header label for the internal storage</extracomment>
+        <translation>外部储存</translation>
+    </message>
+    <message id="harbour-books-storage-book_count" numerus="yes">
+        <source>%0 book(s)</source>
+        <extracomment>Number of books in the storage header</extracomment>
+        <translation>
+            <numerusform>%0 book</numerusform>
+            <numerusform>%0 书籍</numerusform>
+        </translation>
+    </message>
+    <message id="harbour-books-storage-menu-settings">
+        <source>Settings</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>设置</translation>
+    </message>
+    <message id="harbour-books-storage-menu-scan_downloads">
+        <source>Scan downloads</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>扫描下载</translation>
+    </message>
+    <message id="harbour-books-storage-menu-delete_everything">
+        <source>Delete all books</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>删除全部书籍</translation>
+    </message>
+    <message id="harbour-books-storage-view-swipe_left_hint">
+        <source>Swipe left to see what&apos;s on the SD-card</source>
+        <translation>向左轻扫以查看SD卡中的书籍</translation>
+    </message>
+    <message id="harbour-books-import-page-import_n_books" numerus="yes">
+        <source>Import %0 book(s)</source>
+        <extracomment>Dialog header button</extracomment>
+        <translation>
+            <numerusform>Import %0 book</numerusform>
+            <numerusform>输入 %0 书籍</numerusform>
+        </translation>
+    </message>
+    <message id="harbour-books-import-page-select_books">
+        <source>Select books</source>
+        <extracomment>Dialog header button</extracomment>
+        <translation>选择书籍</translation>
+    </message>
+    <message id="harbour-books-import-page-select_all">
+        <source>Select all</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>全选</translation>
+    </message>
+    <message id="harbour-books-import-page-no_new_books_found">
+        <source>No new books found</source>
+        <extracomment>Import page placeholder</extracomment>
+        <translation>未发现新书籍</translation>
+    </message>
+    <message id="harbour-books-book-browser_link-title">
+        <source>Link</source>
+        <extracomment>External link menu title</extracomment>
+        <translation>连接</translation>
+    </message>
+    <message id="harbour-books-book-browser_link-open_in_browser">
+        <source>Open in browser</source>
+        <extracomment>Open link in browser</extracomment>
+        <translation>用浏览器打开</translation>
+    </message>
+    <message id="harbour-books-book-browser_link-copy_to_clipboard">
+        <source>Copy to clipboard</source>
+        <extracomment>Copy link to clipboard</extracomment>
+        <translation>复制链接到剪贴板</translation>
+    </message>
+    <message id="harbour-books-settings-page-header">
+        <source>Books</source>
+        <extracomment>Settings page header</extracomment>
+        <translation>书籍</translation>
+    </message>
+    <message id="harbour-books-settings-page-display-section_header">
+        <source>Display</source>
+        <extracomment>Section header for display settings</extracomment>
+        <translation>显示</translation>
+    </message>
+    <message id="harbour-books-settings-page-font_size_label">
+        <source>Font size</source>
+        <extracomment>Slider label</extracomment>
+        <translation>字体大小</translation>
+    </message>
+    <message id="harbour-books-settings-page-font_size_label-default">
+        <source>Default</source>
+        <extracomment>Slider value label for the standard font size</extracomment>
+        <translation>默认</translation>
+    </message>
+    <message id="harbour-books-settings-page-orientation_label">
+        <source>Orientation</source>
+        <extracomment>Combo box label</extracomment>
+        <translation>方向</translation>
+    </message>
+    <message id="harbour-books-settings-page-orientation-dynamic">
+        <source>Dynamic</source>
+        <extracomment>Combo box value for dynamic orientation</extracomment>
+        <translation>动态</translation>
+    </message>
+    <message id="harbour-books-settings-page-orientation-portrait">
+        <source>Portrait</source>
+        <extracomment>Combo box value for portrait orientation</extracomment>
+        <translation>竖屏</translation>
+    </message>
+    <message id="harbour-books-settings-page-orientation-landscape">
+        <source>Landscape</source>
+        <extracomment>Combo box value for landscape orientation</extracomment>
+        <translation>横屏</translation>
+    </message>
+    <message id="harbour-books-settings-page-page_layout">
+        <source>Page layout</source>
+        <extracomment>Combo box label</extracomment>
+        <translation>页面布局</translation>
+    </message>
+    <message id="harbour-books-settings-page-layout-dynamic">
+        <source>Dynamic</source>
+        <extracomment>Combo box value for dynamic page layout</extracomment>
+        <translation>动态</translation>
+    </message>
+    <message id="harbour-books-settings-page-layout-dynamic-detail">
+        <source>(toggle on tap)</source>
+        <extracomment>Combo box detail for dynamic page layout</extracomment>
+        <translation>(等候切换)</translation>
+    </message>
+    <message id="harbour-books-settings-page-layout-clean">
+        <source>Clean</source>
+        <extracomment>Combo box value for clean page layout (just the content)</extracomment>
+        <translation>清除</translation>
+    </message>
+    <message id="harbour-books-settings-page-layout-clean-detail">
+        <source>(just the content)</source>
+        <extracomment>Combo box detail for clean page layout (just the content)</extracomment>
+        <translation>(仅内容)</translation>
+    </message>
+    <message id="harbour-books-settings-page-layout-minimal">
+        <source>Minimal</source>
+        <extracomment>Combo box value for minimal page layout (title + page)</extracomment>
+        <translation>最小化</translation>
+    </message>
+    <message id="harbour-books-settings-page-layout-minimal-detail">
+        <source>(title, page)</source>
+        <extracomment>Combo box detail for minimal page layout (title + page)</extracomment>
+        <translation>(标题, 页)</translation>
+    </message>
+    <message id="harbour-books-settings-page-layout-normal">
+        <source>Regular</source>
+        <extracomment>Combo box value for normal page layout (title + page + slider)</extracomment>
+        <translation>常规</translation>
+    </message>
+    <message id="harbour-books-settings-page-layout-normal-detail">
+        <source>(title, page, slider)</source>
+        <extracomment>Combo box detail for normal page layout (title + page + slider)</extracomment>
+        <translation>(标题, 页, 滑动条)</translation>
+    </message>
+    <message id="harbour-books-settings-page-layout-full">
+        <source>Full</source>
+        <extracomment>Combo box value for full page layout (title + page + slider)</extracomment>
+        <translation>Full</translation>
+    </message>
+    <message id="harbour-books-settings-page-layout-full-detail">
+        <source>(everything)</source>
+        <extracomment>Combo box detail for full page layout (title + page + slider)</extracomment>
+        <translation>(全部)</translation>
+    </message>
+    <message id="harbour-books-settings-page-keep_display_on">
+        <source>Keep display on while reading</source>
+        <extracomment>Text switch label</extracomment>
+        <translation>阅读时保持显示</translation>
+    </message>
+    <message id="harbour-books-settings-page-keep_display_on_description">
+        <source>Prevent the display from blanking while reading the book.</source>
+        <extracomment>Text switch description</extracomment>
+        <translation>阅读时阻止屏幕熄灭。</translation>
+    </message>
+    <message id="harbour-books-settings-page-navigation-section_header">
+        <source>Navigation</source>
+        <extracomment>Section header for media keys</extracomment>
+        <translation>导航</translation>
+    </message>
+    <message id="harbour-books-settings-page-volume_up-label">
+        <source>Volume up</source>
+        <extracomment>Combo box label</extracomment>
+        <translation>音量增大键</translation>
+    </message>
+    <message id="harbour-books-settings-page-volume_down-label">
+        <source>Volume down</source>
+        <extracomment>Combo box label</extracomment>
+        <translation>音量减小键</translation>
+    </message>
+    <message id="harbour-books-settings-page-action-none">
+        <source>No action</source>
+        <extracomment>Combo box value for no action</extracomment>
+        <translation>无操作</translation>
+    </message>
+    <message id="harbour-books-settings-page-action-previous_page">
+        <source>Previous page</source>
+        <extracomment>Combo box value for previous page action</extracomment>
+        <translation>上一页</translation>
+    </message>
+    <message id="harbour-books-settings-page-action-next_page">
+        <source>Next page</source>
+        <extracomment>Combo box value for next page action</extracomment>
+        <translation>下一页</translation>
+    </message>
+    <message id="harbour-books-settings-page-turn_pages_by_tap">
+        <source>Turn pages by tapping the screen</source>
+        <extracomment>Text switch label</extracomment>
+        <translation>点击屏幕以翻页</translation>
+    </message>
+    <message id="harbour-books-settings-page-turn_pages_by_tap-description">
+        <source>Tapping near the left edge of the screen returns to the previous page, tapping near the right edge gets you to the next page.</source>
+        <extracomment>Text switch description</extracomment>
+        <translation>点击屏幕左侧边缘以返回上一页，点击右边缘附近以切换到下一页。</translation>
+    </message>
+    <message id="harbour-books-settings-page-removable-section_header">
+        <source>Memory card</source>
+        <extracomment>Section header for memory card settings</extracomment>
+        <translation>储存卡</translation>
+    </message>
+    <message id="harbour-books-settings-page-removable_root-label">
+        <source>Books folder</source>
+        <extracomment>Settings field label</extracomment>
+        <translation>书籍文件夹</translation>
+    </message>
+    <message id="harbour-books-settings-page-removable_root-description">
+        <source>Leave the folder name empty to scan the entire memory card for books.</source>
+        <extracomment>Settings field description</extracomment>
+        <translation>不输入文件夹名称以扫描SD卡中的全部书籍<translation>
+    </message>
+</context>
+</TS>
